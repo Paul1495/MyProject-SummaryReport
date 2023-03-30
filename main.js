24797663f097cc3ea140520864f2d8c0f2d284bb
PaulKien <mtrungkien1014@gmail.com>
2023-03-30 13:39:39 +0700

    Update final

---
+++ main.js
@@ -0,0 +1,40 @@
+
+import {
+    description_data,
+    columns_data
+} from './data.js';
+
+var grid = $('#grid').dxDataGrid({
+    dataSource: description_data,
+      keyExpr: 'ID',
+      columns: columns_data,
+      showBorders: true,
+}).dxDataGrid('instance');
+
+var onRowPrepared = function (e) {
+    // console.log(e);
+    if(e.rowType == "header") {
+        e.rowElement.css('background', '#FFBF9B');
+        return;
+    }
+};
+
+grid.option('onRowPrepared', onRowPrepared);
+
+var onCellPrepared = function (e) {
+    console.log(e);
+    e.cellElement.css("text-align", "center");
+    e.cellElement.css("vertical-align", "middle");
+ 
+    if (e.rowType == "header") {
+        e.cellElement.css('font-weight', 'bold');
+        e.cellElement.css('color', '#000000');
+        return;
+    }
+    };
+
+grid.option('onCellPrepared', onCellPrepared);
+
+
+
+
